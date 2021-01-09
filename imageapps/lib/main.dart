f3318fedf5a574fbb942ffd0bd4ce321f4233891
herry88 <herry081288@gmail.com>
2021-01-10 00:08:07 +0700

    AddImagesApps

---
+++ imageapps/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sekolah Program',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        title: new Text('Image Flutter'),
+      ),
+      body: new Center(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'images/flutterlogo.png',
+              width: 200,
+              height: 200,
+            ),
+            Text(
+              'Logo Flutter',
+              style: new TextStyle(
+                fontSize: 15.0,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
