dab3d94eb28b94f3321d2ae926809fc2f8972e99
herry88 <herry081288@gmail.com>
2021-02-25 00:30:14 +0700

    Raisedbutton1

---
+++ raisedbuttonapps/android/app/src/main/kotlin/com/adipramana/raisedbuttonapps/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.adipramana.raisedbuttonapps
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ raisedbuttonapps/lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('RaisedButton'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            new RaisedButton(
+              child: new Text(
+                'Ini Button',
+              ),
+              onPressed: () {
+                print(
+                  'Tulisan ini akan tampil di console ketika di klik',
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
